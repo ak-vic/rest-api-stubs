33280c6fe4cce0f3fa223c245a3ffba65b80f445
Alexander Kuznetsov <Alexander.Kuznetsov@emergn.com>
2020-07-16 18:32:53 +0300

    Added client web service with generated remote web service client - like a proxy for the remote service.

---
+++ AspNetCoreClient/Controllers/UsersProxyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreClient.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreClient.Controllers
+{
+    [ApiController]
+    public class UsersProxyController : ControllerBase
+    {
+        private readonly ILogger<UsersProxyController> _logger;
+        private readonly RemoteService remoteService;
+
+        public UsersProxyController(ILogger<UsersProxyController> logger, RemoteService remoteService)
+        {
+            _logger = logger;
+            this.remoteService = remoteService;
+        }
+
+        [HttpGet, Route("users")]
+        public async Task<ICollection<User>> Get()
+        {
+            return await remoteService.GetAsync();
+        }
+        
+    }
+}
---
+++ AspNetCoreClient/Services/RemoteService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreClient.Services
+{
+    public partial class RemoteService
+    {
+        //Define additional logic here
+    }
+}
