f99cd34a5e59c3261029d412b236b65a52dda6a7
Alexander Kuznetsov <Alexander.Kuznetsov@emergn.com>
2020-07-27 11:17:45 +0300

    initially generated with npx @openapitools/openapi-generator-cli generate -i c:/oas.yaml -g nodejs-express-server -o /openapi-generator

---
+++ Node.js/openapi-generator/controllers/UsersController.js
@@ -0,0 +1,43 @@
+/**
+ * The UsersController file is a very simple one, which does not need to be changed manually,
+ * unless there's a case where business logic reoutes the request to an entity which is not
+ * the service.
+ * The heavy lifting of the Controller item is done in Request.js - that is where request
+ * parameters are extracted and sent to the service, and where response is handled.
+ */
+
+const Controller = require('./Controller');
+const service = require('../services/UsersService');
+const createUser = async (request, response) => {
+  await Controller.handleRequest(request, response, service.createUser);
+};
+
+const deleteUser = async (request, response) => {
+  await Controller.handleRequest(request, response, service.deleteUser);
+};
+
+const getUser = async (request, response) => {
+  await Controller.handleRequest(request, response, service.getUser);
+};
+
+const getUserById = async (request, response) => {
+  await Controller.handleRequest(request, response, service.getUserById);
+};
+
+const getUserByName = async (request, response) => {
+  await Controller.handleRequest(request, response, service.getUserByName);
+};
+
+const updateUser = async (request, response) => {
+  await Controller.handleRequest(request, response, service.updateUser);
+};
+
+
+module.exports = {
+  createUser,
+  deleteUser,
+  getUser,
+  getUserById,
+  getUserByName,
+  updateUser,
+};
---
+++ Node.js/openapi-generator/services/UsersService.js
@@ -0,0 +1,132 @@
+/* eslint-disable no-unused-vars */
+const Service = require('./Service');
+
+/**
+* Create a user
+*
+* user User  (optional)
+* returns User
+* */
+const createUser = ({ user }) => new Promise(
+  async (resolve, reject) => {
+    try {
+      resolve(Service.successResponse({
+        user,
+      }));
+    } catch (e) {
+      reject(Service.rejectResponse(
+        e.message || 'Invalid input',
+        e.status || 405,
+      ));
+    }
+  },
+);
+/**
+* Delete user
+*
+* id Integer 
+* no response value expected for this operation
+* */
+const deleteUser = ({ id }) => new Promise(
+  async (resolve, reject) => {
+    try {
+      resolve(Service.successResponse({
+        id,
+      }));
+    } catch (e) {
+      reject(Service.rejectResponse(
+        e.message || 'Invalid input',
+        e.status || 405,
+      ));
+    }
+  },
+);
+/**
+* Get all users
+*
+* returns List
+* */
+const getUser = () => new Promise(
+  async (resolve, reject) => {
+    try {
+      resolve(Service.successResponse({
+      }));
+    } catch (e) {
+      reject(Service.rejectResponse(
+        e.message || 'Invalid input',
+        e.status || 405,
+      ));
+    }
+  },
+);
+/**
+* Get user by id
+*
+* id Integer 
+* returns User
+* */
+const getUserById = ({ id }) => new Promise(
+  async (resolve, reject) => {
+    try {
+      resolve(Service.successResponse({
+        id,
+      }));
+    } catch (e) {
+      reject(Service.rejectResponse(
+        e.message || 'Invalid input',
+        e.status || 405,
+      ));
+    }
+  },
+);
+/**
+* Get user by name
+*
+* name String 
+* returns User
+* */
+const getUserByName = ({ name }) => new Promise(
+  async (resolve, reject) => {
+    try {
+      resolve(Service.successResponse({
+        name,
+      }));
+    } catch (e) {
+      reject(Service.rejectResponse(
+        e.message || 'Invalid input',
+        e.status || 405,
+      ));
+    }
+  },
+);
+/**
+* Update user
+*
+* id Integer 
+* user User  (optional)
+* no response value expected for this operation
+* */
+const updateUser = ({ id, user }) => new Promise(
+  async (resolve, reject) => {
+    try {
+      resolve(Service.successResponse({
+        id,
+        user,
+      }));
+    } catch (e) {
+      reject(Service.rejectResponse(
+        e.message || 'Invalid input',
+        e.status || 405,
+      ));
+    }
+  },
+);
+
+module.exports = {
+  createUser,
+  deleteUser,
+  getUser,
+  getUserById,
+  getUserByName,
+  updateUser,
+};
